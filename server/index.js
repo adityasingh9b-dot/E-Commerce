e55cc97d628b637865cfe9016ac83ce64d883d2f
adityasingh9b-dot <acpedwardlivingston@gmail.com>
2025-07-18 07:03:44 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ import orderRouter from './route/order.route.js'
 const app = express()
 
 app.use(cors({
-  origin: "http://localhost:5173", // ✅ frontend URL
+  origin: process.env.FRONTEND_URL || "http://localhost:5173",
   credentials: true               // 🔥 allow cookies to be sent!
 }));
 
