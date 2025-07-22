6a49cc4314a4cdc6932ed55d07e9925ea951012e
adityasingh9b-dot <acpedwardlivingston@gmail.com>
2025-07-22 19:33:48 +0530

    Update DisplayCartItem.jsx

--- client/src/components/DisplayCartItem.jsx
+++ client/src/components/DisplayCartItem.jsx
@@ -86,7 +86,7 @@ const DisplayCartItem = ({close}) => {
                                 </div>
                                 <div className='flex gap-4 justify-between ml-1'>
                                     <p>Delivery Charges</p>
-                                    <p className='flex items-center gap-2'>10</p>
+                                    <p className='flex items-center gap-2'>₹10</p>
                                 </div>
                                 <div className='font-semibold flex items-center justify-between gap-4'>
                                     <p >Grand total</p>
