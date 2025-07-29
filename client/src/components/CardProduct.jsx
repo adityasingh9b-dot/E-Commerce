bd0787e5c6ef0777933057f96d28a8d7e99d8cc7
adityasingh9b-dot <acpedwardlivingston@gmail.com>
2025-07-29 14:07:51 +0530

    Update CardProduct.jsx

--- client/src/components/CardProduct.jsx
+++ client/src/components/CardProduct.jsx
@@ -19,7 +19,14 @@ const CardProduct = ({data}) => {
     <Link to={url} className='border py-2 lg:p-4 grid gap-1 lg:gap-3 min-w-36 lg:min-w-52 rounded cursor-pointer bg-white' >
       <div className='min-h-20 w-full max-h-24 lg:max-h-32 rounded overflow-hidden'>
 <img 
-  src={data.image[0].startsWith('http') ? data.image[0] : `https://${data.image[0]}`}
+  src={
+  data.image?.[0]?.startsWith('http') 
+    ? data.image[0] 
+    : data.image?.[0]?.startsWith('//') 
+      ? `https:${data.image[0]}`
+      : `https://${data.image[0]}`
+}
+
   alt={data.name}
   loading="lazy"
   referrerPolicy="origin"
