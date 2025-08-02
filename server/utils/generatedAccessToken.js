1e0f72b6fcdc7b45fb60b026d9db3f7397802995
adityasingh9b-dot <acpedwardlivingston@gmail.com>
2025-08-03 00:41:03 +0530

    Update generatedAccessToken.js

--- server/utils/generatedAccessToken.js
+++ server/utils/generatedAccessToken.js
@@ -1,12 +1,13 @@
 import jwt from 'jsonwebtoken';
 
-const generatedAccessToken = (userId) => {
+const generatedAccessToken = (user) => {
   return jwt.sign(
-    { _id: userId },
+    { _id: user._id, role: user.role },
     process.env.SECRET_KEY_ACCESS_TOKEN,
-    { expiresIn: '15m' }
+    {
+      expiresIn: user.role === "admin" ? "7d" : "2h",
+    }
   );
 };
 
 export default generatedAccessToken;
-
